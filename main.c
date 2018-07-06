90f163e76132af730ad4a033c9d15869d345b28a
Divermv <Divermv@gmail.com>
2018-07-06 13:56:22 +0300

    Select

--- main.c
+++ main.c
@@ -5,8 +5,6 @@ int main (int argc, char *argv[])
 {
 
  printf("%s\n","Server start");
-  TCP_server(3428);// прием и возврат
- // UDP_server(3433);// приемник
- //  UDP_con_server(3426);// приемник
+  server(3428);// прием и возврат
   return 0;
 }
